9f892c060b42b5f41dc40a10f4f182c0caea4a96
Balakrishnan Govindaswamy <Balakrishnan.G@XEBIAINDIA.LOCAL>
2022-11-14 13:40:30 +0530

    Inital Commit

---
+++ src/test/java/com/assess/batch/R2rAssessBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assess.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2rAssessBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
