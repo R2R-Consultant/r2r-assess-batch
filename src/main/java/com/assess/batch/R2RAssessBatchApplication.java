0701353dc852c377d0c78126d9c81cef9a1f7eda
Balakrishnan Govindaswamy <Balakrishnan.G@XEBIAINDIA.LOCAL>
2022-11-16 11:12:21 +0530

    Eureka changes

--- src/main/java/com/assess/batch/R2RAssessBatchApplication.java
+++ src/main/java/com/assess/batch/R2RAssessBatchApplication.java
@@ -2,14 +2,17 @@ package com.assess.batch;
 
 
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class R2RAssessBatchApplication {
 
-	private static final org.apache.logging.log4j.Logger logger = LogManager.getLogger(R2RAssessBatchApplication.class);
+	private static final Logger logger = LogManager.getLogger(R2RAssessBatchApplication.class);
 	
 	public static void main(String[] args) {
 		SpringApplication.run(R2RAssessBatchApplication.class, args);
